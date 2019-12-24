16e1ad66df097b8868d32584e91fae1873119c6c
Mitchell Augustin <mitchell@mitchellaugustin.com>
2019-12-23 18:02:36 -0600

    Initial Commit

---
+++ mainwindow.cpp
@@ -0,0 +1,24 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    //Moves the fontComboBox declared in the UI editor to the toolbar since it can't be done from the editor directly
+    ui->toolBar->addWidget(ui->fontComboBox);
+    ui->toolBar->addWidget(ui->fontSizeSpinner);
+    ui->browserToolBar->addWidget(ui->notebooksListView);
+    ui->browserToolBar->addWidget(ui->sectionsListView);
+
+    //Creates a new textEdit and adds it to the vertical layout.
+    QTextEdit* textEdit = new QTextEdit(this);
+    ui->verticalLayout->addWidget(textEdit);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
